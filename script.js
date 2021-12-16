aa56e9f59b52e76d54075505b39af575e2c8c8b3
0xdrivendev <hxdrivendev@gmail.com>
2021-12-15 21:41:37 -0800

    formatted, set script to type module

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-const provider = new HttpProvider('https://sejong.net.solidwallet.io/api/v3');
+import IconService from "icon-sdk-js";
+
+const provider = new HttpProvider("https://sejong.net.solidwallet.io/api/v3");
 const iconService = new IconService(provider);
 
+console.log("hello");
