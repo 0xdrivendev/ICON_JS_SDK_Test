11a8f2fab7374d3be645bf36e204c5b672898904
0xdrivendev <hxdrivendev@gmail.com>
2021-12-17 13:37:50 -0800

    js debugging environment setup

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const icon_sdk_js_1 = __importDefault(require("icon-sdk-js"));
-const provider = new icon_sdk_js_1.default.HttpProvider('https://sejong.net.solidwallet.io/api/v3');
-const iconService = new icon_sdk_js_1.default(provider);
-var hello = 'Hello ICON World!';
-console.log(iconService.getLastBlock);
+// import IconService from 'icon-sdk-js';
+// const provider = new IconService.HttpProvider('https://sejong.net.solidwallet.io/api/v3');
+// const iconService = new IconService(provider);
+var hello = 'Hello ICON';
+console.log(hello);
+// console.log(iconService.getLastBlock);
--- script.ts
+++ script.ts
@@ -1,6 +1,7 @@
-import IconService from 'icon-sdk-js';
-const provider = new IconService.HttpProvider('https://sejong.net.solidwallet.io/api/v3');
-const iconService = new IconService(provider);
+// import IconService from 'icon-sdk-js';
+// const provider = new IconService.HttpProvider('https://sejong.net.solidwallet.io/api/v3');
+// const iconService = new IconService(provider);
 
-var hello = 'Hello ICON World!';
-console.log(iconService.getLastBlock);
+var hello = 'Hello ICON';
+console.log(hello);
+// console.log(iconService.getLastBlock);
