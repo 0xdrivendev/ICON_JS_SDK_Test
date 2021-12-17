0ebe27fc6fba900e563bde143b99288bab697105
0xdrivendev <hxdrivendev@gmail.com>
2021-12-16 18:48:49 -0800

    converted project to typescript

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
-"use strict";
 //import IconService from "icon-sdk-js";
 //const provider = new HttpProvider("https://sejong.net.solidwallet.io/api/v3");
 //const iconService = new IconService(provider);
+
 var hello = "Hello ICON World!";
 console.log(hello);
